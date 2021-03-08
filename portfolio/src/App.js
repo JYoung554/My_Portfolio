f340d8b31034aeefa87a72b0d3c4698c115dc3fb
JYoung554 <jontefears10@gmail.com>
2021-03-08 17:41:19 -0500

    portfolio

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MainPage from './components/MainPage'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="top">Hi</header>
+      <MainPage/>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/MainPage.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+    function MainPage() {
+      return (
+        <div className="mainPage">
+<h1>Jonte' Fears</h1>
+<br/>
+<h2>Software Engineering Fellow</h2>
+<br/>
+<p>Atlanta Metropolitan Area</p>
+<p>470-594-0187</p>
+<p>jontefears10@gmail.com</p>
+<p>LinkedIn : https://www.linkedin.com/in/jyoung554</p>
+<br/>
+<h3>Projects</h3>
+</div>
+
+    // <Content/>
+      )
+    }
+
+export default MainPage;
+
+
+
+
